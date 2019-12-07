5bf68a08f78457da1ba288ac410ec6011c0d20d5
kDevelop <kat-kole@mail.ru>
2019-12-07 14:51:59 +0300

    update

--- local/templates/.default/components/bitrix/catalog.section.list/catalog-menu/result_modifier.php
+++ local/templates/.default/components/bitrix/catalog.section.list/catalog-menu/result_modifier.php
@@ -13,8 +13,54 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 
-$rootSectionKey = 0;
 $sectionKeyIdRelations = [];
+
+foreach ($arResult["SECTIONS"] as $key => $arSection) {
+    $sectionKeyIdRelations[$arSection["ID"]] = $key;
+}
+
+//подразделы
+$sectionId = [];
+$rsSection = CIBlockSection::GetList(
+    [],
+    [
+        "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+        "ACTIVE" => "Y",
+        "SECTION_ID" => array_column($arResult["SECTIONS"], "ID")
+    ],
+    false,
+    ["ID", "IBLOCK_ID", "IBLOCK_SECTION_ID", "UF_*", "NAME", "SECTION_PAGE_URL", "PICTURE"]
+);
+while ($section = $rsSection->fetch()) {
+    $sectionId[] = $section["ID"];
+    $section["NAME"] = $section["UF_SECTION_LABEL"] ? $section["UF_SECTION_LABEL"] : $section["NAME"];
+    $section["PICTURE"] = CFile::GetFileArray($section["PICTURE"]);
+    $arResult["SECTIONS"][] = $section;
+    if (isset($sectionKeyIdRelations[$section["IBLOCK_SECTION_ID"]])) {
+        $arResult["SECTIONS"][$sectionKeyIdRelations[$section["IBLOCK_SECTION_ID"]]]["CHILD_SECTIONS"][] = count($arResult["SECTIONS"]) - 1;
+        //TODO: убрать
+        $rsSectionInner = CIBlockSection::GetList(
+            [],
+            [
+                "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+                "ACTIVE" => "Y",
+                "SECTION_ID" => $section["ID"]
+            ],
+            false,
+            ["ID", "IBLOCK_ID", "IBLOCK_SECTION_ID", "UF_*", "NAME", "SECTION_PAGE_URL", "PICTURE"]
+        );
+        while ($sectionInner = $rsSectionInner->fetch()) {
+            $sectionInner["NAME"] = $sectionInner["UF_SECTION_LABEL"] ? $sectionInner["UF_SECTION_LABEL"] : $sectionInner["NAME"];
+            $sectionInner["PICTURE"] = CFile::GetFileArray($sectionInner["PICTURE"]);
+            $arResult["SECTIONS"][] = $sectionInner;
+            if (isset($sectionKeyIdRelations[$section["IBLOCK_SECTION_ID"]])) {
+                $arResult["SECTIONS"][$sectionKeyIdRelations[$section["IBLOCK_SECTION_ID"]]]["CHILD_SECTIONS"][] = count($arResult["SECTIONS"]) - 1;
+            }
+        }
+        //end
+    }
+}
+//end
 
 foreach ($arResult["SECTIONS"] as $key => $arSection) {
     $arResult["SECTIONS"][$key]["NAME"] = $arSection["UF_SECTION_LABEL"] ? $arSection["UF_SECTION_LABEL"] : $arSection["NAME"];
@@ -31,28 +77,7 @@ foreach ($arResult["SECTIONS"] as $key => $arSection) {
         }
     }
     //end
-    $sectionKeyIdRelations[$arSection["ID"]] = $key;
 }
-
-//подразделы
-$rsSection = CIBlockSection::GetList(
-    [],
-    [
-        "IBLOCK_ID" => $arParams["IBLOCK_ID"],
-        "ACTIVE" => "Y",
-        "SECTION_ID" => array_column($arResult["SECTIONS"], "ID")
-    ],
-    false,
-    ["ID", "IBLOCK_ID", "IBLOCK_SECTION_ID", "UF_*", "NAME", "SECTION_PAGE_URL"]
-);
-while ($section = $rsSection->fetch()) {
-    $section["NAME"] = $section["UF_SECTION_LABEL"] ? $section["UF_SECTION_LABEL"] : $section["NAME"];
-    $arResult["SECTIONS"][] = $section;
-    if (isset($sectionKeyIdRelations[$section["IBLOCK_SECTION_ID"]])) {
-        $arResult["SECTIONS"][$sectionKeyIdRelations[$section["IBLOCK_SECTION_ID"]]]["CHILD_SECTIONS"][] = count($arResult["SECTIONS"]) - 1;
-    }
-}
-//end
 
 $cp = $this->__component;
 if (is_object($cp)) {
