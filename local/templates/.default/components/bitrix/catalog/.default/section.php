0c8bce3f9be1cec91d4b10a96986a8ae0e3d7563
kDevelop <kat-kole@mail.ru>
2020-01-19 00:22:47 +0300

    fix

--- local/templates/.default/components/bitrix/catalog/.default/section.php
+++ local/templates/.default/components/bitrix/catalog/.default/section.php
@@ -247,7 +247,7 @@ if (!isset($arSection)) {
                             'BRAND_PROPERTY' => (isset($arParams['BRAND_PROPERTY']) ? $arParams['BRAND_PROPERTY'] : ''),
 
                             'TEMPLATE_THEME' => (isset($arParams['TEMPLATE_THEME']) ? $arParams['TEMPLATE_THEME'] : ''),
-                            "ADD_SECTIONS_CHAIN" => "N",
+                            "ADD_SECTIONS_CHAIN" => "Y",
                             'ADD_TO_BASKET_ACTION' => $basketAction,
                             'SHOW_CLOSE_POPUP' => isset($arParams['COMMON_SHOW_CLOSE_POPUP']) ? $arParams['COMMON_SHOW_CLOSE_POPUP'] : '',
                             'COMPARE_PATH' => $arResult['URL_TEMPLATES']['compare'],
